b1551e144716071e714b9f24fe333729586bfdcb
ezdall <67018806+ezdall@users.noreply.github.com>
2022-10-10 09:03:30 +0800

    make this.state for input-link & button-submit

--- src/components/image-link/image-link.comp.jsx
+++ src/components/image-link/image-link.comp.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import './image-link.css';
 
-export default function ImageLink() {
+export default function ImageLink({ onInputChange, onSubmitLink, input }) {
   return (
     <div>
       <p className="f3">This app will detect faces in a picture</p>
 
       <div className="center">
         <div className="center form pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 center" type="text" name="text" />
+          <input
+            className="f4 pa2 w-70 center"
+            type="text"
+            name="text"
+            onChange={onInputChange}
+            value={input}
+          />
           <button
             type="button"
             className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            onClick={onSubmitLink}
           >
             Detect
           </button>
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,7 +5,7 @@ import Particles from 'react-particles-js';
 import Navigation from '../components/navigation/navigation.comp';
 import Logo from '../components/logo/logo.comp';
 import ImageLink from '../components/image-link/image-link.comp';
-import FaceDetect from '../components/face-detect.comp';
+// import FaceDetect from '../components/face-detect.comp';
 
 // style
 import 'tachyons';
@@ -24,16 +24,44 @@ const particleOpts = {
 };
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onSubmitLink = this.onSubmitLink.bind(this);
+  }
   //
-  componentDidMount() {}
+  // componentDidMount() {}
+
+  onInputChange(ev) {
+    const { value } = ev.target;
+
+    // console.log(ev.type);
+    this.setState({ input: value });
+  }
+
+  onSubmitLink(ev) {
+    // const { input } = this.state;
+
+    console.log(ev.type);
+    // this.setState({ imgLink: input });
+  }
 
   render() {
+    const { input } = this.state;
+
     return (
       <div className="App">
         <Particles className="particles" params={particleOpts} />
         <Navigation />
         <Logo />
-        <ImageLink />
+        <ImageLink
+          onInputChange={this.onInputChange}
+          onSubmitLink={this.onSubmitLink}
+          input={input}
+        />
         {/* <FaceDetect /> */}
       </div>
     );

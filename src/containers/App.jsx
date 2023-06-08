1c6eb1a3e912871b5f6a47bf1ab50f2ff72c2ac4
ezdall <67018806+ezdall@users.noreply.github.com>
2023-06-08 16:27:53 +0800

    add & integrate tachyons, react-tilt

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 //
-import Navigation from '../components/navigation.comp';
-import Logo from '../components/logo.comp';
-import ImageLink from '../components/image-link.comp';
+import Navigation from '../components/navigation/navigation.comp';
+import Logo from '../components/logo/logo.comp';
+import ImageLink from '../components/image-link/image-link.comp';
 import FaceDetect from '../components/face-detect.comp';
 
 // style
+import 'tachyons';
 import './App.css';
 
 class App extends React.Component {

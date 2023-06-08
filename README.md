04aadeb003f2f3714df0cdef9542fe5f52b8ba15
ezdall <67018806+ezdall@users.noreply.github.com>
2023-06-08 11:53:53 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Face Detect
+
+- react
+- redux - @reduxjs/toolkit
+- react-redux
+- bootstrap
+- eslint, airbnb, prettier - formatter
+- gh-pages - deploy
---
+++ src/containers/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+//
+// import SearchBox from '../components/search-box.comp';
+// import CardList from '../components/card-list.comp';
+// import ErrorBoundry from '../components/error-boundry.comp';
+
+// style
+import './App.css';
+
+class App extends React.Component {
+  //
+  componentDidMount() {}
+
+  render() {
+    // console.log(this.props);
+
+    return <div>App</div>;
+  }
+}
+export default App;

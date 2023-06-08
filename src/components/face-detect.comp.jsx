685a039f96e9d5d2265cf37109ab885910c57eaf
ezdall <67018806+ezdall@users.noreply.github.com>
2023-06-08 12:15:09 +0800

    add logo, navi, image, face-detect. init comps

---
+++ src/components/face-detect.comp.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function FaceDetect() {
+  return <div>Face detect</div>;
+}
---
+++ src/components/image-link.comp.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ImageLink() {
+  return <div className="">ImageLink</div>;
+}

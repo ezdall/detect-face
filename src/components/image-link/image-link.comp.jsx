6400407c55340fcc262ef684eed977aa21431939
ezdall <67018806+ezdall@users.noreply.github.com>
2023-06-20 17:26:38 +0800

    add onLoad on <img>, load image before face detect

--- src/components/image-link/image-link.comp.jsx
+++ src/components/image-link/image-link.comp.jsx
@@ -7,7 +7,7 @@ export default function ImageLink({ onInputChange, onSubmitLink, input }) {
       <p className="f3">This app will detect face in a picture</p>
 
       <div className="center">
-        <div className="css-pattern center pa4 br3 shadow-5" >
+        <div className="css-pattern center pa4 br3 shadow-5">
           <input
             className="f4 pa2 w-70 center"
             type="text"
@@ -15,11 +15,10 @@ export default function ImageLink({ onInputChange, onSubmitLink, input }) {
             placeholder="Enter image link/source"
             onChange={onInputChange}
             value={input}
-            onKeyDown={(ev) => {
-              if(ev.key !== 'Enter'){
+            onKeyDown={ev => {
+              if (ev.key !== 'Enter') {
                 return null;
               }
-              console.log('trigger')
               return onSubmitLink(ev);
             }}
           />
